2648539ea64caf4859b43bdb546e15ff30767355
Amit <amit.kaneria@gmail.com>
2017-04-07 23:21:56 -0400

    adding different dataformat support for javaproperties/json/concise binary/csv/xml

--- dataformat/src/main/java/jackson/v1/CsvTest.java
+++ dataformat/src/main/java/jackson/v1/CsvTest.java
@@ -1,5 +1,6 @@
 package jackson.v1;
 
+import com.fasterxml.jackson.databind.ObjectReader;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvSchema;
@@ -14,20 +15,18 @@ public class CsvTest {
         try {
             User user = UserUtil.getUserInstance();
 
-            CsvSchema schema = mapper.schemaFor(User.class).withColumnSeparator(',');
+            CsvSchema schema = mapper.schemaFor(User.class).withHeader().withColumnSeparator(',');
             ObjectWriter objectWriter = mapper.writer(schema);
             System.out.println("Write ... ... ... ");
             System.out.println(objectWriter.writeValueAsString(user));
 
             System.out.println("Read ... ... ... ");
-            String userYaml = "firstName=Amit\n" +
-                    "lastName=Kaneria\n" +
-                    "address.1=130 Laidlaw Ave\n" +
-                    "address.2=Jersey City\n" +
-                    "address.3=NJ 07307";
+            String userYaml = "firstName,lastName\n" +
+                    "Amit,Kaneria";
 
-            UserAddress newUserAddress = mapper.readValue(userYaml, UserAddress.class);
-            System.out.println(newUserAddress);
+            ObjectReader objectReader = mapper.reader(schema);
+            User newUser = objectReader.readValue(userYaml);
+            System.out.println(newUser);
         } catch (Exception e) {
             e.printStackTrace();
         }
