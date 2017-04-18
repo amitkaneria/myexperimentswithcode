fdb011edca4f624ca63a1f2c1bbff2e6296410d9
Amit <amit.kaneria@gmail.com>
2017-04-17 21:33:10 -0400

    rabbit mq tests

---
+++ messaging/src/main/java/rabbitmq/client/Sender.java
@@ -0,0 +1,20 @@
+package rabbitmq.client;
+
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+
+import java.util.concurrent.TimeoutException;
+
+public class Sender {
+
+    private final static String QUEUE_NAME = "hello";
+
+    public static void main(String[] argv)
+            throws java.io.IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+    }
+}
